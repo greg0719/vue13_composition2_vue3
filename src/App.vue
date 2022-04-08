30f2018214d1db443f6670dea52ca31faa55de8a
greg0719 <greglee@fareastone.com.tw>
2022-04-08 17:03:04 +0800

    composition

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section id="lab13">
+    <h3>{{ course }}</h3>
+    <h3>{{ price }}</h3>
+    <button @click="extraDuration">bad way</button>
+    <h3>{{ displayCourseName }}</h3>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive, ref, computed } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  setup() {
+    const course = reactive({ name: "POOP", duration: 35 });
+    const price = ref(24000);
+    const courseName = "hello";
+    const courseFullName = "vue";
+    const displayCourseName = computed(function () {
+      return courseName + " " + courseFullName;
+    });
+    function extraDuration() {
+      course.duration += 7;
+      price.value += 2000;
+    }
+    return { course, extraDuration, price, displayCourseName };
+  },
+};
 </script>
 
 <style>
